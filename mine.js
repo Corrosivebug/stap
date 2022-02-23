1a0bbe8383fa4dc6758688c7700a74e1165647ff
Corrosivebug <92105717+Corrosivebug@users.noreply.github.com>
2022-02-23 19:23:38 +0200

    tabel_stepeny
    2 simester

---
+++ mine.js
@@ -0,0 +1,10 @@
+alert("нажмите F12")
+for(let i = 1; i<11; i++){
+    console.log(`Таблица степеней на ${i}`)
+    console.log("--------")
+for(let j = 1; j<11; j++ ){
+        console.log(`${i} в степени ${j} равняется ${i**j}`)
+    }
+    console.log("--------")
+}
+
